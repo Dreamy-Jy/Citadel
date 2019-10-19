9602eb5f0110ba21f5fbdfbd221e19ed710cda61
Jordane Thomas <thomasjordane7@gmail.com>
2019-10-19 14:33:43 -0400

    constants update

--- mobile/index.js
+++ mobile/index.js
@@ -4,15 +4,22 @@
 
 import App from './App';
 import { Navigation } from "react-native-navigation";
-import AsyncStorage from "@react-native-community/async-storage";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { 
+    faPlus, 
+    faChevronLeft, 
+    faRedoAlt, 
+    faFileSignature, 
+    faPencilAlt, 
+    faMonument 
+} from "@fortawesome/free-solid-svg-icons";
+import { 
+    faEye, 
+    faTrashAlt
+} from "@fortawesome/free-regular-svg-icons";
 
-storeData = async () => {
-    try {
-        await AsyncStorage.setItem('@storage_key', 'stored value')
-    } catch (e) {}
-}
-storeData();
+library.add(faPlus, faChevronLeft, faRedoAlt, faFileSignature, faPencilAlt, faMonument, faEye, faTrashAlt);
 
 Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
 
--- mobile/src/constants.js
+++ mobile/src/constants.js
@@ -1,10 +1,30 @@
-const backgroundColorOne = "#022169";
-const backgroundColorTwo = "#FAFEFF";
+export const backgroundColor_Dark = "rgba(2, 33, 106, 1)";
+export const mainFontColor_Dark = "rgba(255, 255, 255, 1)";
+export const actionColor_Dark = "rgba(154, 210, 255, 1)";
+export const highlightColor_Dark = "rgba(255, 255, 255, .17)"
 
-const textColorOne = "#FFFFFF";
-const textColorTwo = "#022169";
-const textColorThree = "#0072CE";
-const textColorFour = "#000000";
 
-const fontFamilyOne = "";
+export const backgroundColor_Light = "rgba(255, 255, 255, 1)";
+export const mainFontColor_Light = "rgba(2, 33, 106, 1)";
+export const actionColor_Light = "rgba(0, 114, 206, 1)";
+export const highlightColor_Light = "rgba(154, 210, 255, .27)"
 
+
+export const placeholderFontColor = "rgba(152, 152, 152, 1)";
+
+
+export const actionFontFamily = "OpenSans-Bold";
+export const headerFontFamily = "Merriweather-Black";
+export const subheaderFontFamily = "Merriweather-Bold";
+export const normalTextFontFamily = "Merriweather-Regular";
+export const inputTextFontFamily = "OpenSans";
+
+
+export const addIcon = "plus";
+export const backArrowIcon = "chevron-left";
+export const remakeIcon = "redo-alt";
+export const saveIcon = "file-signature";
+export const editIcon = "pencil-alt";
+export const appIcon = "monument";
+export const viewIcon = "eye";
+export const deleteIcon = "trash-alt";
